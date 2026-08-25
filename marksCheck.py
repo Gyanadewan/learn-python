c2975b016dbbdec115631830011604ae162627e3
Gyanadewan <gyanadewan1@gmail.com>
2026-08-25 07:10:59 +0600

    marksCheck program

---
+++ marksCheck.py
@@ -0,0 +1,17 @@
+n = int (input("Enter the marks:"))
+if n >100:
+    print ("Dont't be over samrt your marks are not above 100")
+elif n >= 80:
+    print("You got A+")
+elif n>= 70:
+    print ("You got A")
+elif n>= 60 :
+    print ("You got A-")
+elif n>= 50 :
+    print ("You got B")
+elif n >= 40:
+    print ("You got C")
+elif n>=33:
+    print ("You got D")
+else :
+    print ("You  are fail, try again to have a better result")
